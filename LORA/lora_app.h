1ebb25cffda0b2e1970704347eb0260720883401
wdy-615-lenovo <wudongyu1994@foxmail.com>
2018-11-10 12:56:17 +0800

    cmt1110: tran_dir is ok

---
+++ LORA/lora_app.h
@@ -0,0 +1,31 @@
+#ifndef _LORA_APP_H_
+#define _LORA_APP_H_
+
+#include "sys.h"
+#include "lora_cfg.h"
+
+#define LORA_AUX  PAin(4)    //LORA模块状态引脚
+#define LORA_MD0  PAout(5)  //LORA模块控制引脚
+
+extern _LoRa_CFG LoRa_CFG;
+extern u8 Lora_mode;
+
+#define MY_ADDRH	0X00			//记录自己的地址
+#define MY_ADDRL	0X01
+#define MY_ADDR 	((u16)(MY_ADDRH<<8))+MY_ADDRL
+#define OBJ_ADDRH  	0xee     		//记录目标地址
+#define OBJ_ADDRL  	0xee     		
+#define OBJ_ADDR 	((u16)(OBJ_ADDRH<<8))+OBJ_ADDRL
+#define OBJ_CHN 	LORA_CHN        //记录目标信道
+
+u8 LoRa_Init(void);
+void Aux_Int(u8 mode);
+void LoRa_Set(void);
+void LoRa_SendData(u8 addh,u8 addl,u8 chn,u8* Dire_Date);
+void LoRa_ReceData(void);
+void LoRa_Process(void);
+void Lora_Test(void);
+u8 lora_send_cmd(u8 *cmd,u8 *ack,u16 waittime);
+u8* lora_check_cmd(u8 *str);
+
+#endif
---
+++ USER/main.c
@@ -0,0 +1,58 @@
+#include "sys.h"
+#include "led.h"
+#include "delay.h"
+#include "usart.h"
+#include "timer.h"
+#include "exti.h"
+#include "lora_app.h"
+#include "usart3.h"
+
+/************************************************
+逻辑摘要
+    1.初始化lora模块
+    2.lora模块设置（设置过程波特率必须为115200，设置完后波特率设为自定义波特率）
+    3.每1s，给lora模块发送数据（通讯时波特率为自定义波特率）
+
+
+************************************************/
+
+int main(void)
+{
+    u8 hour=1,min=2,locx=5,locy=6,crc=0;
+			//		0,	1,	 2,	  3,	   4,		5,	 6,	 7,	  8,   9,  10
+    u8 sendData[]={0x80,0x07,0x11,MY_ADDRH,MY_ADDRL,hour,min,locx,locy,crc,0x81};
+u8 len= sizeof(sendData)/sizeof(sendData[0]);
+
+//	SystemInit();
+    delay_init();
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    uart1_init(115200);
+    printf("uart1_init success!\n");
+    LED_Init();
+
+    while(LoRa_Init())  //初始化ATK-LORA-01模块,若初始化失败则300ms后重试，直到成功
+    {
+        printf("LoRa undetected...\n");
+        delay_ms(300);
+    }
+    printf("LoRa detected!\n");
+    
+    LoRa_Set();     //LoRa配置(进入配置需设置串口波特率为115200)
+
+    printf("start while(1)\n");
+printf("add= %d %d %d %d %d\n",MY_ADDRH,MY_ADDRL,OBJ_ADDRH,OBJ_ADDRL,len);
+    while(1)
+    {
+        /*if(flag_thing_done){
+            //获取两个RFID的坐标信息
+            locx=getLocx();
+            locy=getLocy();
+            sendData[7]=locx;
+            sendData[8]=locy;
+            LoRa_SendData(OBJ_ADDRH,OBJ_ADDRL,OBJ_CHN,sendData);            //发送数据
+        }*/
+		sendData[9]++;
+        LoRa_SendData(OBJ_ADDRH,OBJ_ADDRL,OBJ_CHN,sendData);            //发送数据
+        delay_ms(1000);
+    }
+}
